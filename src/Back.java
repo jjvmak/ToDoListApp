009ce29b25a86f17706fcb4dc68c189fffa79967
Joonas Mäkinen <makinen.jns@gmail.com>
2017-08-28 15:41:39 +0300

    Commiting previously done work.

---
+++ src/Back.java
@@ -0,0 +1,77 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class Back {
+	
+	private Gui gui;
+	private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd"+"."+"MM"+"."+"yyyy");
+	private LocalDate localDate = LocalDate.now();
+	
+	private ArrayList<String> tasks = new ArrayList<>();
+	
+	public Back() {
+		gui = new Gui(this);
+		System.out.println(dtf.format(localDate));
+		deSerializeReservationsList();
+		loadTasks();
+		gui.setLabel();
+	}
+	
+	public static void main(String[] args) {
+		new Back();
+	}
+	
+	public String getDate() {
+		return dtf.format(localDate);
+	}
+	
+	public void addTask(String s) {
+		tasks.add(s);
+	}
+	
+	public void removeTask(int i) {
+		tasks.remove(i);
+	}
+	
+	public void loadTasks() {
+		for (int i = 0; i < tasks.size(); i++) {
+			gui.loadTask(tasks.get(i));
+		}
+	}
+	
+	public void serializeTaskList() {
+		FileOutputStream fileOut;
+		try {
+			fileOut = new FileOutputStream("tasks.ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(tasks);
+			out.close();
+			fileOut.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	public void deSerializeReservationsList() {
+		try {
+			FileInputStream fileIn = new FileInputStream("tasks.ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			tasks = (ArrayList<String>) in.readObject();
+			in.close();
+			fileIn.close();
+		}catch(IOException i) {
+			System.out.println("The file is not yet created.");
+
+		}catch(ClassNotFoundException c) {
+
+			c.printStackTrace();
+		}
+	}
+}
---
+++ src/Gui.java
@@ -0,0 +1,113 @@
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.List;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+
+public class Gui {
+
+	private JFrame frame;
+	private JPanel mainPanel;
+	private List list;
+	private JTextField editorPane;
+	private JButton btnSubmit;
+	private Back back;
+	private JButton btnDone;
+	private JLabel itemsOnListLabel;
+
+	public Gui(Back back) {
+		this.back = back;
+		initialize();
+		
+	}
+	
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 700, 450);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		mainPanel = new JPanel();
+		mainPanel.setBounds(0, 0, 692, 423);
+		frame.getContentPane().add(mainPanel);
+		mainPanel.setLayout(null);
+		
+		list = new List();
+		list.setBounds(10, 24, 672, 338);
+		mainPanel.add(list);
+		
+		
+		editorPane = new JTextField();
+		editorPane.setBounds(10, 373, 467, 28);
+		mainPanel.add(editorPane);
+		
+		btnSubmit = new JButton("Add");
+		btnSubmit.setBounds(487, 373, 91, 29);
+		mainPanel.add(btnSubmit);
+		
+		btnDone = new JButton("Done");
+		btnDone.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				onDone();
+			}
+		});
+		btnDone.setBounds(588, 373, 91, 29);
+		mainPanel.add(btnDone);
+		
+		itemsOnListLabel = new JLabel("New label");
+		itemsOnListLabel.setBounds(10, 4, 672, 14);
+		mainPanel.add(itemsOnListLabel);
+		btnSubmit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				onSubmit();
+			}
+		});
+		frame.setVisible(true);
+	}
+	
+	public void onSubmit() {
+		
+		if (editorPane.getText().length()>0) {
+			String text = back.getDate()+" "+editorPane.getText();
+			list.add(text);
+			editorPane.setText("");
+			setLabel();
+			back.addTask(text);
+			back.serializeTaskList();
+		}
+		
+		
+	}
+	
+	public void onDone() {
+		
+		if (isSomethingSelected()) {
+			int selectedItem = list.getSelectedIndex();
+			list.remove(selectedItem);
+			setLabel();
+			back.removeTask(selectedItem);
+			back.serializeTaskList();
+		}
+	}
+	
+	public boolean isSomethingSelected() {
+		
+		for (int i = 0; i < list.getItemCount(); i++) {
+			if (list.isIndexSelected(i)) return true;
+		}
+		
+		return false;
+	}
+	
+	public void setLabel() {
+		itemsOnListLabel.setText("Total: "+list.getItemCount()+" tasks");
+	}
+	
+	public void loadTask(String s) {
+		list.add(s);
+	}
+}
